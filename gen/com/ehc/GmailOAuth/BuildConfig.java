5ff6cfbdf69a5252e12f27d2aa284dc7c534f47d
chennakeshav <keshav@eggheadcreative.com>
2014-07-11 13:58:14 +0530

    initial commit

---
+++ gen/com/ehc/GmailOAuth/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.ehc.GmailOAuth;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
